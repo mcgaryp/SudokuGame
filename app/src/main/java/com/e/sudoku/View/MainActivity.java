99f341697f7d07e9a12bcd15307b50ed04dcbe9f
mcgaryp <mcg15003@byui.edu>
2019-12-12 19:53:47 -0700

    Update MainActivity.java

--- app/src/main/java/com/e/sudoku/View/MainActivity.java
+++ app/src/main/java/com/e/sudoku/View/MainActivity.java
@@ -3,6 +3,7 @@ package com.e.sudoku.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.e.sudoku.Presenter.Manager.MainManager;
 import com.e.sudoku.R;
@@ -10,12 +11,14 @@ import com.e.sudoku.R;
 public class MainActivity extends AppCompatActivity {
     // VARIABLES
     private MainManager manager;
+    private EditText block00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         manager = new MainManager();
+
     }
 
 }
